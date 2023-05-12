f7ba35dd4c9dfaccef15a68a74d3bf07d288148e
jcondoripin <jcondoripin@unsa.edu.pe>
2023-05-12 15:21:24 -0500

    imprimir nombre

---
+++ imprimirNombre.java
@@ -0,0 +1,7 @@
+public class imprimirNombre{
+	public static void main(String[} args) {
+		String name = "Juan Jose";
+		String lastName = "Condori Pinto";
+		System.out.println(name + " " + lastName);
+	}
+}
