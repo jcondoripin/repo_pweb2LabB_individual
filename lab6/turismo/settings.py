e53632fc9f75e747cb6f0ac4c3c9d88b937995a0
JuanJoseCP <juanjosecondoripinto@gmail.com>
2023-07-02 12:46:52 -0500

    Lab6 - template header

--- lab6/turismo/settings.py
+++ lab6/turismo/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'destinos.apps.DestinosConfig'
+    'destinos.apps.DestinosConfig',
+    'bootstrap5',
 ]
 
 MIDDLEWARE = [
