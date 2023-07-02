39bc696d6c854a0b39f361592da01bc9674905a0
JuanJoseCP <juanjosecondoripinto@gmail.com>
2023-07-02 14:55:44 -0500

    lab6 - models and migrations

--- lab6/destinos/models.py
+++ lab6/destinos/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Destino(models.Model):
+
+    nombre = models.TextField(blank=False, unique=True, null=False, help_text="Nombre de esta ciudad de destino turístico", max_length=50)
+
+    descripcion = models.TextField(blank=False, null=False, help_text="Descripción de esta ciudad de destino turístico")
+
+    imagen = models.ImageField(upload_to="static/images/", help_text="Imagen de esta ciudad de destino turístico")
+
+    precioTour = models.FloatField(blank=False, null=False, help_text="Precio del tour a esta ciudad de destino turístico")
+
+    oferta = models.BooleanField(default=False, null=False, blank=False, help_text="Decide si el viaje a este destino turístico está de oferta")
