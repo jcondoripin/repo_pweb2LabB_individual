66a7b4ec4b8f371aeeca5cf9e96250e6783d0a89
JuanJoseCP <juanjosecondoripinto@gmail.com>
2023-07-14 21:39:55 -0500

    Lab07 - migracion a bd local

--- lab7/relations/settings.py
+++ lab7/relations/settings.py
@@ -77,8 +77,14 @@ WSGI_APPLICATION = 'relations.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'relationsDjango',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3306'
     }
 }
 
