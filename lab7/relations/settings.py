2ea75b4e952391157b593cec46317c6e2272a354
JuanJoseCP <juanjosecondoripinto@gmail.com>
2023-07-15 10:59:53 -0500

    seguridad gmail borrado

--- lab7/relations/settings.py
+++ lab7/relations/settings.py
@@ -134,36 +134,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 # Configuraciones para email
-
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'juanjosecondoripinto@gmail.com'
-EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-EMAIL_HOST_PASSWORD = 'szdbovdtzeqsgnsl'
