137332eb645302a4ba2351dc0501513a6ad3fda9
JuanJoseCP <juanjosecondoripinto@gmail.com>
2023-07-14 23:30:11 -0500

    Lab07 - send email index

--- lab7/test_email/views.py
+++ lab7/test_email/views.py
@@ -4,7 +4,7 @@ from django.core.mail import send_mail
 # Create your views here.
 def index(request):
     send_mail('Hola de parte de juan jose',
-              'Mensaje bla bla bla',
+              'Mensaje por parte del servidor de django',
               'juanjosecondoripinto@gmail.com',
               ['rolado5567@niback.com'],
               fail_silently=False)
