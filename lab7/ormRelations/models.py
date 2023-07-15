b09cb9eadc4d83d7ef624ede7ef296aa1496b655
JuanJoseCP <juanjosecondoripinto@gmail.com>
2023-07-14 21:20:38 -0500

    Lab07 - many to many models

--- lab7/ormRelations/models.py
+++ lab7/ormRelations/models.py
@@ -1,9 +1,27 @@
 from django.db import models
 
-# Create your models here.
+# Relación uno a muchos
 class Language(models.Model):
     name = models.CharField(max_length=25)
 
 class Framework(models.Model):
     name = models.CharField(max_length=25)
     language = models.ForeignKey(Language, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Framework {self.name}"
+
+# Relación muchos a muchos
+class Student(models.Model):
+    name = models.CharField(max_length=25)
+    age = models.IntegerField(null=False)
+
+    def __str__(self) -> str:
+        return self.name
+
+class School(models.Model):
+    name = models.CharField(max_length=50)
+    students = models.ManyToManyField(Student)
+
+    def __str__(self) -> str:
+        return self.name
