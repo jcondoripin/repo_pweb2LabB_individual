cbedfa825291f7480a55dd336eba7a594523325b
JuanJoseCP <juanjosecondoripinto@gmail.com>
2023-07-25 20:30:06 -0500

    lab8 - routes and components

--- lab8/Frontend/src/app/app.module.ts
+++ lab8/Frontend/src/app/app.module.ts
@@ -4,11 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
+import { IndexRouterComponent } from './views/index-router/index-router.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { FormComponent } from './components/form/form.component';
+import { TableViewComponent } from './views/table-view/table-view.component';
+import { AboutViewComponent } from './views/about-view/about-view.component';
+import { TableShowViewComponent } from './views/table-show-view/table-show-view.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexRouterComponent,
+    HeaderComponent,
+    FooterComponent,
+    FormComponent,
+    TableViewComponent,
+    AboutViewComponent,
+    TableShowViewComponent
   ],
   imports: [
     BrowserModule,
---
+++ lab8/Frontend/src/app/views/table-show-view/table-show-view.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-table-show-view',
+  templateUrl: './table-show-view.component.html',
+  styleUrls: ['./table-show-view.component.css']
+})
+export class TableShowViewComponent {
+
+}
