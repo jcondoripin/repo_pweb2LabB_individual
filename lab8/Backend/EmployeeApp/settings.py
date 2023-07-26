bce5f318dd45417ae703a1580defbb1faa0853d8
JuanJoseCP <juanjosecondoripinto@gmail.com>
2023-07-25 21:04:37 -0500

    lab8 - cors

--- lab8/Backend/EmployeeApp/settings.py
+++ lab8/Backend/EmployeeApp/settings.py
@@ -38,13 +38,15 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'employees',
-    'rest_framework'
+    'rest_framework',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -70,6 +72,8 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'EmployeeApp.wsgi.application'
+
+CORS_ALLOWED_ORIGINS = ['http://localhost:4200']
 
 
 # Database
